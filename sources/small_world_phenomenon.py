ba5ca31de85eb952e8d6b69436203346eca9dca5
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2022-04-30 23:32:43 +0200

    Refactoring

--- sources/small_world_phenomenon.py
+++ sources/small_world_phenomenon.py
@@ -3,22 +3,18 @@ from json.encoder import INFINITY
 from typing import Tuple
 
 import numpy as np
-from BreadthFirstPaths import BreadthFirstPaths
+from sources.BreadthFirstPaths import BreadthFirstPaths
 from Graph import Graph
 from connected_components import get_bigest_component_vertexes
 
 
-
-
-
-
 def small_world_phenomenon(graph: Graph) -> list():
+    distances = [0, 0]
     vertexes = get_bigest_component_vertexes(graph)
-    bfs = BreadthFirstPaths(graph,vertexes)
+    bfs = BreadthFirstPaths(graph, vertexes)
 
-    
     for v in vertexes:
         if bfs.hasPathTo(v):
-            print(f'{}-{}')
+            print(f'{v}-{v}')
 
     return distances
